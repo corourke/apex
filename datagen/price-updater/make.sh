d828f27704ed039a54c9be74db9e6fd223309e1a
Cameron O'Rourke <cameron.orourke@gmail.com>
2025-04-10 13:12:39 -0700

    Fix start scripts

--- datagen/price-updater/make.sh
+++ datagen/price-updater/make.sh
@@ -1,6 +1,2 @@
 mvn clean package
 cp ./target/price-updater-1.0-jar-with-dependencies.jar ./price-updater-1.0.jar
-if [ ! -f config.properties ] ; 
-  then cp ./src/main/java/acme/config.properties . 
-  chmod 600 config.properties
-fi
--- datagen/scangen/start.sh
+++ datagen/scangen/start.sh
@@ -1,9 +1,9 @@
 if [ ! -f config.properties ] ; 
-  then cp config.properties.example config.properties
+  then cp config_example.properties config.properties
   chmod 600 config.properties
   echo "Please edit config.properties as needed before running the program" 
+  exit
 fi
-
 if [ `uname -s` = "Linux" ]; then
   sudo timedatectl set-timezone America/Los_Angeles
 fi
